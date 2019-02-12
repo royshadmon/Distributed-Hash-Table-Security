98117ef792737b867eeca7cdbd8378ef345bc5df
Alex Williamson <alswilli@ucsc.edu>
2019-02-11 16:51:48 -0800

    added pseudocode for tracker functions

--- src/Trackers/Tracker.java
+++ src/Trackers/Tracker.java
@@ -19,6 +19,69 @@ public class Tracker implements ChordTracker {
         return nextId;
     }
 
+    /* Insert
+    *
+    * Gets selected node from cache queue
+    *
+    * Takes a resource and the assigned resource key from the node as input
+    *
+    * Partition is the IDA, assignID portions
+    *
+    * List will have partitionID, partition
+    *
+    * - selected = NodeQueue.pop()
+    * - list = this.partition(resource)
+    * - keymap.put for each element in list
+    * - selected.insert-> (for all elements in list)    *
+    *
+    *
+    *
+    * */
+
+    /* Partition
+    *
+    * Takes in the resource
+    *
+    * Calls IDA on resource (which also does all the encoding work)
+    *
+    * - listOfPartitions = IDA(resource)
+    * - return listOfPartitions
+    *
+    *
+    *
+    * */
+
+
+
+    /* Lookup
+    *
+    * Gets selected node from the cache queue
+    * Takes in a key from the requesting Node
+    * Tracker finds key in hash table (keymap)
+    * For each key, query -> gets the partitions -> adds each partition and its key to a list
+    * Call inverse on list
+    * Returns resource to node (encryted? PKC?)
+    *
+    * - selected = NodeQueue.pop()
+    * - listOfPartitionKeys = keymap.get(resourceKey)
+    * - listOfPartitions = query (for each partition key)
+    * - resource = inverse(listOfPartitons)
+    * - return resource
+    *
+    * */
+
+    /* */
+
+    /* */
+
+    /* */
+
+    /* */
+
+    /* */
+
+    /* */
+
 
 
 
